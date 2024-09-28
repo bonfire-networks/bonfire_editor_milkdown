de11da9b9c0b77cc1825b4bfcd3e7817c21eccd7
Mayel de Borniol <git@mayel.space>
2024-09-28 09:48:27 +0100

    surface

--- lib/web/components/composer/milkdown.ex
+++ lib/web/components/composer/milkdown.ex
@@ -3,8 +3,6 @@ defmodule Bonfire.Editor.Milkdown do
 
   use Bonfire.UI.Common.Web, :stateless_component
 
-  # alias Surface.Components.Form.TextInput
-
   # use Bonfire.UI.Common.Web, :stateless_component
   # use Bonfire.Common.Utils
 
@@ -13,6 +11,7 @@ defmodule Bonfire.Editor.Milkdown do
   prop textarea_class, :css_class, default: "w-full md:min-h-[280px] h-full"
   prop reset_smart_input, :boolean, default: false
   prop showing_within, :atom
+
   # needed by apps to use this editor to know how to process text they receive from it
   def output_format, do: :markdown
 end
